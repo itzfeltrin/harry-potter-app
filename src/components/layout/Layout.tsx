bd534c906b04698e7a24a611ad4a34ad22041506
Pedro Feltrin <pedrofeltrin45@gmail.com>
2024-08-26 15:32:48 -0300

    style navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from "react";
 
+import classes from "./Layout.module.scss";
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main>
-      <nav>Select your favorite house</nav>
-      <section>{children}</section>
+    <main className={classes.main}>
+      <nav className={classes.nav}>
+        <img src="/assets/imgs/harry-potter.svg" className={classes.logo} />
+        <strong className={classes.title}>Wizard World</strong>
+      </nav>
+      <section className={classes.content}>{children}</section>
     </main>
   );
 }
